57f0544bc5b1b45e4534c8c9f85db1106c706941
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-06-02 21:08:27 +0200

    Set webpack config file for working with css files, fonts and images, everything working well by far. Rewrite scss code will handle with next project, this time i will add watch, live server, eslint options

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('everything works fine');
+import './css/style.css';
+import testImage from './images/testImage.jpg';
+
+const content = document.querySelector('#content');
+console.log(content);
+const mainHeader = document.createElement('h1');
+mainHeader.textContent = 'Welcome to our restaurant!';
+const secondaryHeader = document.createElement('h2');
+secondaryHeader.textContent = 'This is the secondary Header!';
+const container = document.createElement('div');
+container.classList.add('container');
+const firstImage = new Image();
+firstImage.src = testImage;
+
+container.appendChild(firstImage);
+content.appendChild(mainHeader);
+content.appendChild(secondaryHeader);
+content.appendChild(container);
