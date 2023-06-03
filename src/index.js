1b6ca129d9fbbdb27213727114bb86d0af35b288
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-06-03 11:23:28 +0200

    Style menu bar, add buttons and hover effects on it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import './css/style.css';
+import './css/reset.css';
 
 const content = document.querySelector('#content');
 
 const container = document.createElement('div');
+const restaurantName = document.createElement('h1');
+const menu = document.createElement('div');
+const menuOptions = ['Home', 'Menu', 'Contact'];
+menuOptions.forEach((option) => {
+  const el = document.createElement('button');
+  el.textContent = option;
+  menu.appendChild(el);
+});
+menu.classList.add('menu');
+
+restaurantName.textContent = 'Spicy Heaven';
 container.classList.add('container');
+container.appendChild(restaurantName);
+container.appendChild(menu);
 content.appendChild(container);
